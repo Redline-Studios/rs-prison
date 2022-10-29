0e465958375f70a57b4c79939deef55986269245
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2022-10-29 16:12:24 -0600

    Better Workouts

--- client/workout.lua
+++ client/workout.lua
@@ -1,51 +1,31 @@
-local hasWorkedOut = false
+local sleep = 1000
+local resting = false
+local training = false
 
-RegisterNetEvent("prison:chinUp", function()
-    if not hasWorkedOut then
+RegisterNetEvent("qb-prison:client:DoChinUp", function()
+    if not training and not resting then
         Hang()
     else
-        antiSpam()
+        QBCore.Functions.Notify('You\'re resting', 'primary')
     end
 end)
 
 CreateThread(function()
-    exports['qb-target']:AddBoxZone("PrisonChinUp", vector3(1745.91, 2481.26, 45.74), 1, 2.0, {
-        name = "PrisonChinUp",
-        heading = 30,
-        debugPoly = false,
-        minZ = 45.74 - 1,
-        maxZ = 45.74 + 1,
-    }, {
-        options = {
-            {
-                type = "client",
-                event = "prison:chinUp",
-                icon = "fas fa-dumbbell",
-                label = "Do Chin-Ups",
-                canInteract = function()
-                    if inJail then
-                        return true
-                    else 
-                        return false
-                    end
-                end,
-            },
-        },
-        distance = 1.5
-    })
+	while true do
+		if resting then
+			Wait(Config.Workout.Cooldown * 1000)
+			resting = false
+			training = false
+			QBCore.Functions.Notify('You caught your breath. You can workout again!', 'success')
+		end
+		Wait(sleep)
+	end
 end)
 
-function antiSpam()
-    if Config.Delay >= 1 then 
-        hasWorkedOut = true
-        Wait(1000 * Config.Delay)
-        QBCore.Functions.Notify( Lang:t("error.workout_breather"), "error")
-    end
-end
-
 function Hang()
-    if not hasWorkedOut then
-        QBCore.Functions.Progressbar('doing_hang', 'Doing Pullups', 10000, false, false, {
+    if not training and not resting then
+        training = true
+        QBCore.Functions.Progressbar('doing_hang', 'Doing Pullups', (Config.Workout.Chinup.time * 1000), false, false, {
             disableMovement = true, --
             disableCarMovement = true,
             disableMouse = false,
@@ -54,8 +34,10 @@ function Hang()
             animDict = "amb@prop_human_muscle_chin_ups@male@base",
             anim = "base",
             flags = 8,
-        }, {}, {}, function()  
-            QBCore.Functions.Notify("Work out completed", "success")  
+        }, {}, {}, function()
+            training = false
+            resting = true
+            QBCore.Functions.Notify("Work out completed", "success")
             ClearPedTasks(PlayerPedId())
 	    	exports['ps-buffs']:AddArmorBuff(60000, 5)
 	    	exports['ps-buffs']:StaminaBuffEffect(80000, 1.2)
@@ -64,14 +46,16 @@ function Hang()
             TriggerEvent('inventory:client:busy:status', false)
             QBCore.Functions.Notify("Just Breathe First..", "error")
         end, "fas fa-dumbbell")
+    else
+        QBCore.Functions.Notify('You\'re resting', 'primary')
     end
 end
 
-exports['qb-target']:AddTargetModel({-1978741854, 2057317573, -232023078}, { --Yoga mats
+exports['qb-target']:AddTargetModel({-1978741854, 2057317573, -232023078}, {
     options = {
         {
             type = "client",
-            event = "yoga:start",
+            event = "qb-prison:client:DoYoga",
             icon = "fas fa-yin-yang",
             label = "Do yoga",
         },
@@ -79,29 +63,39 @@ exports['qb-target']:AddTargetModel({-1978741854, 2057317573, -232023078}, { --Y
     distance = 2.5
 })
 
-RegisterNetEvent('yoga:start', function()
+RegisterNetEvent('qb-prison:client:DoYoga', function()
     local ped = PlayerPedId()
     local coords = GetEntityCoords(ped)
-    local finish = exports['ps-ui']:Circle(function(success)
-        if success then
-            print("success")
-        else
-            print("fail")
-        end
-    end, 2, 20) -- NumberOfCircles, MS
 
-
-    FreezeEntityPosition(ped, true)
-    if finish then
-        ClearPedTasks(ped)
-        TriggerEvent('animations:client:EmoteCommandStart', {"yoga"})
-        FreezeEntityPosition(ped, false)
-        TriggerServerEvent('hud:server:RelieveStress', 5)
-        QBCore.Functions.Notify("You have a bit less stress", "success")
+    if not training and not resting then
+        FreezeEntityPosition(ped, true)
+        exports['ps-ui']:Circle(function(success)
+            if success then
+                TriggerEvent('animations:client:EmoteCommandStart', {"yoga"})
+                QBCore.Functions.Progressbar('doing_yoga', 'Doing Yoga', (Config.Workout.Yoga.time * 1000), false, false, {
+                    disableMovement = true,
+                    disableCarMovement = true,
+                    disableMouse = false,
+                    disableCombat = true,
+                }, {}, {}, {}, function()
+                    training = false
+                    resting = true
+                    ClearPedTasks(ped)
+                    FreezeEntityPosition(ped, false)
+                    TriggerServerEvent('hud:server:RelieveStress', Config.Workout.Yoga.stress)
+                    QBCore.Functions.Notify("You have a bit less stress", "success")
+                end, function()
+                    TriggerEvent('inventory:client:busy:status', false)
+                    QBCore.Functions.Notify("Just Breathe First..", "error")
+                end)
+            else
+                ClearPedTasks(ped)
+                FreezeEntityPosition(ped, false)
+                QBCore.Functions.Notify("Failed, focus..", "error")
+                SetPedToRagdollWithFall(ped, 1000, 2000, 1, GetEntityForwardVector(ped), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+            end
+        end, Config.Workout.Yoga.Minigame.circles, Config.Workout.Yoga.Minigame.trime)
     else
-        ClearPedTasks(ped)
-        FreezeEntityPosition(ped, false)
-        QBCore.Functions.Notify("Failed, focus..", "error")
-        SetPedToRagdollWithFall(ped, 1000, 2000, 1, GetEntityForwardVector(ped), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        QBCore.Functions.Notify('You\'re resting', 'primary')
     end
 end)
